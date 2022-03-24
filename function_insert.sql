33758f2939a6cbc9bcf8623f4c4db685faabf3c0
Tania Salazar Trujillo <74995863+tsatru@users.noreply.github.com>
2022-03-23 22:36:42 -0600

    Update function_insert.sql

--- function_insert.sql
+++ function_insert.sql
@@ -1,7 +1,9 @@
-		CREATE OR REPLACE FUNCTION add_comments(specimen_id INT4, campo TEXT, comentario TEXT) 
+CREATE OR REPLACE FUNCTION add_comments(specimen_id INT4, campo TEXT, comentario TEXT) 
     RETURNS void AS $$
     BEGIN
       INSERT INTO produccion.store_comments (specimen_id, campo, comentario) VALUES ($1, $2, $3);
     END;
     $$ LANGUAGE plpgsql;
 		
+--la función se invoca de la siguiente manera:
+--SELECT add_comments('13', 'family', 'algo');
